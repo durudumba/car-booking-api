e322dbe619078c2c71d39441a187ae9afd41a4f1
ksh <dia2445@raontec.co.kr>
2024-09-30 18:27:30 +0900

    사용신청 개발완료 / 로그인 개발중

--- src/main/java/com/raontec/carbookingapi/api/CarController.java
+++ src/main/java/com/raontec/carbookingapi/api/CarController.java
@@ -1,14 +1,13 @@
 package com.raontec.carbookingapi.api;
 
 import com.raontec.carbookingapi.data.CarDAO;
+import com.raontec.carbookingapi.vo.BookAppFormVO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -17,13 +16,14 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 @CrossOrigin("*")
-@RequestMapping("/api/car")
+@RestController
+@RequestMapping("/api/car/")
 public class CarController {
 
     private final CarDAO carDAO;
 
-    @GetMapping(value="/selectCarList", produces = {"application/json"})
-    public List<Map<String, String>> selectCarList() {
-        return carDAO.selectCarList();
+    @GetMapping(value="selectCarList", produces = {"application/json"})
+    public List<Map<String, String>> selectCarList(@RequestParam Map<String, String> param) {
+        return carDAO.selectCarList(param);
     }
 }
--- src/main/java/com/raontec/carbookingapi/data/CarDAO.java
+++ src/main/java/com/raontec/carbookingapi/data/CarDAO.java
@@ -1,5 +1,6 @@
 package com.raontec.carbookingapi.data;
 
+import com.raontec.carbookingapi.vo.BookAppFormVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.Map;
 @Mapper
 public interface CarDAO {
 
-    List<Map<String, String>> selectCarList();
+    List<Map<String, String>> selectCarList(Map<String, String> map);
 }
